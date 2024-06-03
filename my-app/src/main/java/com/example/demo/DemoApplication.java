6566c9f25046c666c2fee19a3762b0d15015ec6d
emberow <chin45630@gmail.com>
2024-06-03 15:38:30 +0800

    feat: add serverlet to run API server

--- my-app/src/main/java/com/example/demo/DemoApplication.java
+++ my-app/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+        SpringApplication application = new SpringApplication(DemoApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  springboot 啟動!!   ლ(´ڡ`ლ)ﾞ");
 	}
 
 }
