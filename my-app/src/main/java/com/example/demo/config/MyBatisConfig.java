1a575b3cb0b216d352ceccdc27301c983e595b9f
emberow <chin45630@gmail.com>
2024-06-15 13:06:28 +0800

    fix: remove unnecessary code

--- my-app/src/main/java/com/example/demo/config/MyBatisConfig.java
+++ my-app/src/main/java/com/example/demo/config/MyBatisConfig.java
@@ -4,7 +4,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
@@ -15,8 +14,6 @@ public class MyBatisConfig {
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
-        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver()
-                .getResources("classpath:mapper/*.xml"));
         return sessionFactory.getObject();
     }
 }
