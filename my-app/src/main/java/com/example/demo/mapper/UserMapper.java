368550d096eabc4b93ee2fbaf177b82305d9d270
emberow <chin45630@gmail.com>
2024-06-18 09:54:52 +0800

    fix: lint

--- my-app/src/main/java/com/example/demo/mapper/UserMapper.java
+++ my-app/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,9 +1,14 @@
 package com.example.demo.mapper;
 
 import com.example.demo.entity.User;
-import org.apache.ibatis.annotations.*;
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface UserMapper {
